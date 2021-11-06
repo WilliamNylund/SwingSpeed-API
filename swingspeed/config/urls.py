4ab9395b7ad55e267658c02d6fb7d0c9b12a87c3
WilliamNylund <william.nylund@gmail.com>
2021-11-07 00:37:50 +0200

    Include swings url into config/urls

--- swingspeed/config/urls.py
+++ swingspeed/config/urls.py
@@ -19,6 +19,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('', include('users.urls')),
+    path('api/', include('users.urls')),
+    path('api/', include('swings.urls')),
 ]
 #urlpatterns = format_suffix_patterns(urlpatterns)
