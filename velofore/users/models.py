9b638a5da26fe6aa84d6703baeae65c0f4bc21bb
WilliamNylund <william.nylund@gmail.com>
2022-01-27 19:03:11 +0200

    Add age, height, handicap, lefty and gender fields to user model

--- velofore/users/models.py
+++ velofore/users/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
-
+from django.core.validators import MinValueValidator, MaxValueValidator
 from .managers import UserManager
 
 def user_directory_path(instance, filename):
@@ -10,12 +10,22 @@ def user_directory_path(instance, filename):
     return 'profile-pictures/user_{0}/{1}'.format(instance.id, filename)
 
 class User(AbstractBaseUser, PermissionsMixin):
+    class Gender(models.IntegerChoices):
+        MALE = 0
+        FEMALE = 1
+        OTHER = 2
+    
     username = models.CharField(_('username'), unique=True, max_length=20, blank=False)
     email = models.EmailField(_('email address'), unique=True, blank=True, null=True)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     date_created = models.DateTimeField(default=timezone.now)
     profile_picture = models.ImageField(upload_to=user_directory_path, blank=True)
+    age = models.PositiveSmallIntegerField(null=True, validators=[MinValueValidator(0), MaxValueValidator(150)])
+    gender = models.IntegerField(choices=Gender.choices, null=True)
+    height = models.FloatField(null=True)
+    handicap = models.PositiveSmallIntegerField(null=True, validators=[MinValueValidator(0), MaxValueValidator(36)])
+    lefty = models.BooleanField(null=True)
 
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = []
@@ -24,3 +34,5 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.username
+
+    
--- velofore/users/serializers.py
+++ velofore/users/serializers.py
@@ -1,12 +1,14 @@
 from django.contrib.auth import get_user_model
-from .models import User
+from swings.models import Swing
 from rest_framework import serializers
-
+from django.db.models import Max
 
 class UserSerializer(serializers.ModelSerializer):
+    #gender = serializers.CharField(source='get_gender_display') #display gender as string
+    top_speed = serializers.SerializerMethodField()
     class Meta:
         model = get_user_model()
-        fields = ['id', 'username', 'email', 'password', 'is_staff', 'is_active', 'is_superuser', 'date_created', 'profile_picture']
+        fields = ['id', 'username', 'email', 'password', 'is_staff', 'is_active', 'is_superuser', 'date_created', 'profile_picture', 'age', 'gender', 'height', 'handicap','lefty', 'top_speed']
         extra_kwargs = {
             'date_created': {'read_only': True},
             'password': {'write_only': True},
@@ -21,6 +23,11 @@ class UserSerializer(serializers.ModelSerializer):
         elif ('is_superuser' in validated_data):
             raise serializers.ValidationError("Contact the database administrator to update is_superuser")
         return super().update(instance, validated_data)
+
+    def get_top_speed(self, user):
+        swings = Swing.objects.filter(user=user)
+        top_speed = swings.aggregate(Max('speed'))
+        return top_speed.get('speed__max')
 
 class UserProfilePictureSerializer(serializers.ModelSerializer):
     class Meta:
