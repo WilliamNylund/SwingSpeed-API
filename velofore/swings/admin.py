0b236e9e4adbed9f1ee5ce3e7f70b4cbc5c19727
WilliamNylund <william.nylund@gmail.com>
2022-02-24 20:27:33 +0200

    Add swings admin panel

--- velofore/swings/admin.py
+++ velofore/swings/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Swing
 
-# Register your models here.
+
+class SwingAdmin(admin.ModelAdmin):
+    model = Swing
+    list_display = ('user', 'speed', 'date_created')
+    list_filter = ('user', 'speed', 'date_created')
+
+    fieldsets = (
+        (None, {'fields': ('date_created', 'user', 'speed', 'is_active', 'note',  'recording')}),
+    )
+    
+    readonly_fields = ('date_created',)
+admin.site.register(Swing, SwingAdmin)
