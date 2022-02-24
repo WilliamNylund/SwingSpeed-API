a0df05a1699b3ba3edde04f3d0596884f588e38d
WilliamNylund <william.nylund@gmail.com>
2022-02-24 20:27:14 +0200

    Users admin improvements

--- velofore/users/admin.py
+++ velofore/users/admin.py
@@ -1,18 +1,14 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-
-from .forms import UserCreationForm, UserChangeForm
 from .models import User
 
 
 class UserAdmin(UserAdmin):
-    add_form = UserCreationForm
-    form = UserChangeForm
     model = User
-    list_display = ('username', 'is_staff', 'is_active',)
-    list_filter = ('username', 'is_staff', 'is_active',)
+    list_display = ('username', 'is_staff', 'is_active')
+    list_filter = ('username', 'is_staff', 'is_active')
     fieldsets = (
-        (None, {'fields': ('username', 'password')}),
+        (None, {'fields': ('username', 'password', 'email', 'age', 'gender', 'height', 'handicap', 'lefty', 'date_created', 'profile_picture')}),
         ('Permissions', {'fields': ('is_staff', 'is_active')}),
     )
     add_fieldsets = (
@@ -21,6 +17,7 @@ class UserAdmin(UserAdmin):
             'fields': ('username', 'password1', 'password2', 'is_staff', 'is_active')}
         ),
     )
+    readonly_fields = ('date_created',)
     search_fields = ('username',)
     ordering = ('username',)
 
